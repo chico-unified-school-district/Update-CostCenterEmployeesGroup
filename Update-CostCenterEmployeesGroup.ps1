cc48b54e621a9248d2cd0ed2e8f126bb90e8a439
Justin Cooper <jcooper@chicousd.org>
2022-09-26 13:42:51 -0700

    added files

--- Update-CostCenterEmployeesGroup.ps1
+++ Update-CostCenterEmployeesGroup.ps1
@@ -1,4 +1,4 @@
-<# 
+<#
 .SYNOPSIS
 Update membership of the Employee-Cost-Center group to keep licensing under 2500 users.
 Scans users who are members of groups beginning with "CN=UN-*"
@@ -9,29 +9,36 @@ Scans users who are members of groups beginning with "CN=UN-*"
 .NOTES
 #>
 [cmdletbinding()]
-param ( 
+param (
  [Parameter(Position = 0, Mandatory = $True)]
- [Alias('DC')]
- [ValidateScript( { Test-Connection -ComputerName $_ -Quiet -Count 1 })]
- [string]$DomainController,
+ [Alias('DCs')]
+ [string[]]$DomainControllers,
  [Parameter(Position = 1, Mandatory = $True)]
- [Alias('ADCred')]
- [System.Management.Automation.PSCredential]$Credential,
+ [System.Management.Automation.PSCredential]$ADCredential,
  [Parameter(Position = 3, Mandatory = $false)]
+ [Alias('wi')]
  [SWITCH]$WhatIf
 )
 
-$adCmdLets = 'Get-ADUser', 'Get-ADGroupMember', 'Add-ADGroupMember', 'Remove-ADGroupMember'
-$adSession = New-PSSession -ComputerName $DomainController -Credential $Credential
-Import-PSSession -Session $adSession -Module ActiveDirectory -CommandName $adCmdLets -AllowClobber > $null
-
 # Import Functions
 . .\lib\Add-Log.ps1
+. .\lib\Clear-SessionData.ps1
+. .\lib\New-ADSession.ps1
+. .\lib\Select-DomainController.ps1
+. .\lib\Show-TestRun.ps1
+
+Show-TestRun
+Clear-SessionData
+
+$dc = Select-DomainController $DomainControllers
+$adCmdLets = 'Get-ADUser', 'Get-ADGroupMember', 'Add-ADGroupMember', 'Remove-ADGroupMember'
+New-ADSession -dc $dc -cmdlets $adCmdLets -cred $ADCredential
+
 
 $userOU = 'OU=Domain_Root,DC=chico,DC=usd'
 # Filter out 'New Employee Accounts' OU and no UN-* group members
 $userObjs = Get-ADUser -Filter * -SearchBase $userOU -Properties 'memberof' |
-Where-Object { ($_.memberof -match "CN=UN-*") -and 
+Where-Object { ($_.memberof -match "CN=UN-*") -and
  ($_.distinguishedName -notlike "*New Employee Accounts*") }
 
 $group = 'Cost-Center-Employees'
@@ -45,7 +52,7 @@ foreach ($item in $comparedObjects) {
  if ($item.SideIndicator -eq "=>") {
   Add-Log remove ('{0},{1}' -f $group, $item.SamAccountName) $WhatIf
   Remove-ADGroupMember -Identity $group -Members $item.SamAccountName -Confirm:$false -WhatIf:$WhatIf
- } 
+ }
  elseif ($item.SideIndicator -eq "<=") {
   Add-Log add ('{0},{1}' -f $group, $item.SamAccountName) $WhatIf
   Add-ADGroupMember -Identity $group -Members $item.SamAccountName -Confirm:$false -WhatIf:$WhatIf
@@ -54,5 +61,5 @@ foreach ($item in $comparedObjects) {
 $groupMembers = Get-ADGroupMember -Identity $group
 Add-Log total ('{0},{1} members' -f $group, $groupMembers.count) $WhatIf
 
-Add-Log cleanup 'Tearing down sessions...'
-Get-PSSession | Remove-PSSession -WhatIf:$WhatIf
+Clear-SessionData
+Show-TestRun
